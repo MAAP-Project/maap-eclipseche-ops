7ac67aede5aaf921c951f9e394b547b476faed5f
bsatoriu <brian.p.satorius@jpl.nasa.gov>
2022-09-01 16:21:49 -0700

    Update extractcert.sh

--- edsc-proxy/extractcert.sh
+++ edsc-proxy/extractcert.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 # Extracts the letsencrypt certs from kubernetes
-microk8s.kubectl get secret default-tls-secret -o yaml | grep -P '^\s+tls.crt:' | sed 's/^.*: //' | base64 -d > /home/ubuntu/kubessl/default-tls-secret.crt
-microk8s.kubectl get secret default-tls-secret -o yaml | grep -P '^\s+tls.key:' | sed 's/^.*: //' | base64 -d > /home/ubuntu/kubessl/default-tls-secret.key
+CRTFILE="/home/ubuntu/kubessl/default-tls-secret.crt"
+KEYFILE="/home/ubuntu/kubessl/default-tls-secret.key"
+microk8s.kubectl get secret default-tls-secret -o yaml | grep -P '^\s+tls.crt:' | sed 's/^.*: //' | base64 -d > "$CRTFILE.tmp"
+microk8s.kubectl get secret default-tls-secret -o yaml | grep -P '^\s+tls.key:' | sed 's/^.*: //' | base64 -d > "$KEYFILE.tmp"
+
+if [ -s "$CRTFILE.tmp" ] && [ -s "$KEYFILE.tmp" ]
+then
+    mv "$CRTFILE.tmp" "$CRTFILE"
+    mv "$KEYFILE.tmp" "$KEYFILE"
+    sudo service apache2 graceful >/dev/null 2>&1
+fi
