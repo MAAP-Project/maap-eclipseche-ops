a4da18cc7c0cfcaff1c98e4f0cbf8608e3237d17
bsatoriu <brian.p.satorius@jpl.nasa.gov>
2022-11-16 15:20:45 -0800

    Fix bug in ebs-snapshot-cleanup.sh

--- cron-scripts/ebs-snapshot-cleanup.sh
+++ cron-scripts/ebs-snapshot-cleanup.sh
@@ -11,7 +11,7 @@ fi
 ct=0
 MAX_SNAPSHOTS=7
 
-for snapshot in $(/usr/local/bin/aws ec2 describe-snapshots --region us-west-2 --filters Name=volume-id,Values=$VOLUME_ID | jq .Snapshots[].SnapshotId | sed 's/\"//g')
+for snapshot in $(/usr/local/bin/aws ec2 describe-snapshots --region us-west-2 --filters Name=volume-id,Values=$VOLUME_ID --query 'Snapshots[*].[StartTime,SnapshotId]' --output text | sort -r | sed 's/\"//g' | awk '{print $2}')
 do
     if (( $ct >= $MAX_SNAPSHOTS ));
     then
